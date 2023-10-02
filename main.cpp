2e44f4f463ceac44c165c9823df77eab17b73492
沖山 <okiyama1609@outlook.jp>
2023-10-02 12:38:20 +0900

    02_04

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 #include <cstdio>
 #include <cstdlib>
 #include <DirectXTex.h>
+#include <assert.h>
+#include "TextureConverter.h"
+
+enum Argument
+{
+	ApplicationPath,
+	FilePath,
+
+	NumArgument
+};
 
 int main(size_t argc,char* argv[])
 {
-	for (size_t i = 0; i < argc; i++)
-	{
-		printf(argv[i]);
+	assert(argc >= NumArgument);
 
-		printf("\n");
-	}
+	//COMライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	//テクスチャコンバータ
+	TextureConverter converter;
+
+
+	//テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[FilePath]);
+
+
+	//COMライブラリの終了
+	CoUninitialize();
 
 	system("pause");
 
