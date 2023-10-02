83852cd61645dfb29839724d16ffafd508f8c14c
沖山 <okiyama1609@outlook.jp>
2023-10-02 11:50:17 +0900

    directXtex

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <cstdio>
 #include <cstdlib>
+#include <DirectXTex.h>
 
-int main(int argc,char* argv[])
+size_t main(size_t argc,char* argv[])
 {
-	for (int i = 0; i < argc; i++)
+	for (size_t i = 0; i < argc; i++)
 	{
 		printf(argv[i]);
 
