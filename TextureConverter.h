ed6a994563510653973db110f286922f17117442
沖山 <okiyama1609@outlook.jp>
2023-10-02 14:51:32 +0900

    02_05

--- TextureConverter.h
+++ TextureConverter.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include<DirectXTex.h>
 
 class TextureConverter
 {
@@ -19,11 +20,34 @@ private:
 	void LoadWICTextureFromFile(const std::string& filePath);
 
 	/// <summary>
+	/// フォルダパスとファイル名を分離する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
+
+	/// <summary>
 	/// マルチバイト文字列をワイド文字列に変換
 	/// </summary>
 	/// <param name="mString">マルチバイト文字列</param>
 	/// <returns>ワイド文字列</returns>
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+	/// <summary>
+	/// DDSテクスチャとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
+
+private:
+
+	DirectX::TexMetadata metadata_;
+
+	DirectX::ScratchImage scratchImage_;
+
+	std::wstring directoryPath_;
+
+	std::wstring fileName_;
+	
+	std::wstring fileExt_;
 
 };
 
